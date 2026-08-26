c5fbdaa6825d2e6f0d25c9d33ec94b23fa8c93df
Maiko.T.S <maiko.t.s.ofc@gmail.com>
2026-08-25 22:33:13 -0300

    Add AppModel class for application representation

--- app/src/main/java/MKLAUNCHER/model/AppModel.java
+++ app/src/main/java/MKLAUNCHER/model/AppModel.java
@@ -1,1 +1,15 @@
-AppModel.java
+package MKLAUNCHER;
+
+import android.graphics.drawable.Drawable;
+
+public class AppModel {
+    public String label;
+    public String packageName;
+    public Drawable icon;
+
+    public AppModel(String label, String packageName, Drawable icon) {
+        this.label = label;
+        this.packageName = packageName;
+        this.icon = icon;
+    }
+}
